aeeee6e4cb1d9d22624de20edb2e4c23d5650458
oskarsoderbom <70432770+oskarsoderbom@users.noreply.github.com>
2023-04-03 20:08:56 +0200

    added som

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from OptionFunctions import EuropeanCall, EuropeanPut
 
 app = Flask(__name__)
@@ -35,7 +35,27 @@ def options():
                              time_to_expiration=time, 
                              risk_free_rate=rate)
 
+        option_price = option.price
+        delta = option.delta
+        gamma = option.gamma
+        vega = option.vega
+
+        return redirect(f"/results?option_price={option_price}&delta={delta}&gamma={gamma}&vega={vega}")
+        
     return render_template('results.html', option=option)
+
+
+@app.route('/results')
+def results():
+    option_price = request.args.get('option_price')
+    delta = request.args.get('delta')
+    gamma = request.args.get('gamma')
+    vega = request.args.get('vega')
+
+    if not option_price or not delta or not gamma or not vega:
+        return redirect('/')
+
+    return render_template('results.html', option_price=option_price, delta=delta, gamma=gamma, vega=vega)
 
 
 if __name__ == '__main__':
